f41cb18e0c1c1f4a9cba2ea9cb12dcaec7a35091
Volodymyr Medvid <vmedvid@riseup.net>
2020-05-23 18:39:41 +0100

    Update usb-audio-device-freertos to version 1.1.0

--- app/usb-audio-device-freertos.cmake
+++ app/usb-audio-device-freertos.cmake
@@ -3,44 +3,48 @@ psoc6_check_bsp(
   CY8CKIT-062-WIFI-BT
   CY8CKIT-062S2-43012
   CY8CPROTO-062-4343W
+  CYW9P62S1-43012EVB-01
+  CYW9P62S1-43438EVB-01
 )
 
 project(usb-audio-device-freertos)
 
 psoc6_load_application(
   NAME mtb-example-psoc6-usb-audio-device-freertos
-  VERSION 1.0.0
+  VERSION 1.1.0
 )
 psoc6_add_executable(
   SOURCES
-    ${APP_DIR}/main.c
-    ${APP_DIR}/audio.h
-    ${APP_DIR}/audio_app.h
-    ${APP_DIR}/audio_app.c
-    ${APP_DIR}/audio_feed.h
-    ${APP_DIR}/audio_feed.c
-    ${APP_DIR}/audio_in.h
-    ${APP_DIR}/audio_in.c
-    ${APP_DIR}/audio_out.h
-    ${APP_DIR}/audio_out.c
-    ${APP_DIR}/rtos.h
-    ${APP_DIR}/touch.h
-    ${APP_DIR}/touch.c
-    ${APP_DIR}/usb_comm.h
-    ${APP_DIR}/usb_comm.c
+    ${APP_DIR}/include/audio.h
+    ${APP_DIR}/include/audio_app.h
+    ${APP_DIR}/include/audio_feed.h
+    ${APP_DIR}/include/audio_in.h
+    ${APP_DIR}/include/audio_out.h
+    ${APP_DIR}/include/rtos.h
+    ${APP_DIR}/include/touch.h
+    ${APP_DIR}/include/usb_comm.h
+    ${APP_DIR}/source/audio_app.c
+    ${APP_DIR}/source/audio_feed.c
+    ${APP_DIR}/source/audio_in.c
+    ${APP_DIR}/source/audio_out.c
+    ${APP_DIR}/source/main.c
+    ${APP_DIR}/source/touch.c
+    ${APP_DIR}/source/usb_comm.c
     ${APP_DIR}/COMPONENT_AK4954A/ak4954a.h
     ${APP_DIR}/COMPONENT_AK4954A/ak4954a.c
+  INCLUDE_DIRS
+    ${APP_DIR}/include
+    ${APP_DIR}/COMPONENT_AK4954A
   LINK_LIBRARIES
     usbdev
-    capsense
     psoc6hal
     freertos
+    abstraction-rtos
+    clib-support
   DESIGN_MODUS
     ${APP_DIR}/COMPONENT_CUSTOM_DESIGN_MODUS/TARGET_${BSP_NAME}/design.modus
   DESIGN_USBDEV
     ${APP_DIR}/COMPONENT_CUSTOM_DESIGN_MODUS/TARGET_${BSP_NAME}/design.cyusbdev
-  DESIGN_CAPSENSE
-    ${APP_DIR}/COMPONENT_CUSTOM_DESIGN_MODUS/TARGET_${BSP_NAME}/design.cycapsense
   GENERATED_SOURCES
     cycfg.h
     cycfg.c
