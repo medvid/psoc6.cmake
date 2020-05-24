63c63022bf362c083877a98b405187cd6ec2fd40
Volodymyr Medvid <vmedvid@riseup.net>
2020-05-24 10:24:12 +0300

    mtb_03_ex02_psoc6_capsense_gestures_linearslider: check BSPs

--- app/mtb_03_ex02_psoc6_capsense_gestures_linearslider.cmake
+++ app/mtb_03_ex02_psoc6_capsense_gestures_linearslider.cmake
@@ -1,6 +1,18 @@
 # This recipe is included from app/community.cmake
 # APP_DIR points to the top-level repo directory
 
+# This application provides custom design.modus for the below boards
+psoc6_check_bsp(
+  CY8CKIT-062-BLE
+  CY8CKIT-062-WIFI-BT
+  CY8CKIT-062S2-43012
+  # BUG: GeneratedSource doesn't provide CYBSP_LED_RGB_RED/GREEN/BLUE
+  #CY8CPROTO-062-4343W
+  CY8CPROTO-062S3-4343W
+  CYW9P62S1-43012EVB-01
+  CYW9P62S1-43438EVB-01
+)
+
 psoc6_add_executable(
   NAME
     mtb_03_ex02_psoc6_capsense_gestures_linearslider
